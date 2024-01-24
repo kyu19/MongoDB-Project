a9d7762b895ba5557aabc4698cbfdcaa216b3944
JP <tripleh1902@hotmail.fr>
2024-01-24 17:43:44 +0100

    Finition du streamlit

--- Livrable/app.py
+++ Livrable/app.py
@@ -23,7 +23,7 @@ from pymongo.server_api import ServerApi
 uri = "mongodb+srv://boobacar5252:ZbsNiFG2mvM2MHcl@clusteryanis.iemddda.mongodb.net/?retryWrites=true&w=majority"
 
 # Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = MongoClient(uri, tls=True, tlsAllowInvalidCertificates=True, server_api=ServerApi('1'))
 
 # Send a ping to confirm a successful connection
 try:
@@ -36,7 +36,7 @@ cursor = client['IPSSI']['projetMongo']
 
 #open Videsogames.csv
 
-df = pd.read_csv('C:\\Users\\ACER NITRO\\Desktop\\MongoDB-Project\\Video_Games.csv', index_col='index')
+df = pd.read_csv('C:\\Users\\33619\\Desktop\\IPSSI_COURS\\NoSQL\\PROJET_Mongo\\MongoDB-Project\\Video_Games.csv', index_col='index')
 
 
 # warnings.filterwarnings('ignore')
@@ -96,6 +96,9 @@ def main():
             background: white;
             padding: 10px;
         }
+        .e1nzilvr5 p {
+            color: white;
+        }
         
     </style>
     """
@@ -106,15 +109,15 @@ def main():
     # st.markdown("<h1 class='centered-title'>Video Games Sales Analysis</h1>", unsafe_allow_html=True)
 
     # Ajouter un titre avant la selectbox
-    st.write("Sélectionnez un graphique dans la liste ci-dessous :")
+    # st.write("Sélectionnez un graphique dans la liste ci-dessous :")
 
     # Selectbox en bas pour afficher les graphiques
-    graphique = st.selectbox('Sélectionnez une option:', options=['graphique1', 'graphique2', 'graphique3', 'graphique4', 'graphique5', 'graphique6', 'graphique7'],
+    graphique = st.selectbox('Sélectionnez un graphique dans la liste ci-dessous :', options=['Total Sales by Genre', 'Sales by Game Types by Region', 'Sales by region', 'Top 10 Platforms by Global Sales', 'Boxplot of Global Sales by Genre', 'Distribution of Release Years', 'Average Global Sales per Region for each Genre'],
                             key='selectbox')  # Ajoutez un identifiant 'key' pour éviter les problèmes de mise en cache
 
     # Logique pour afficher les graphiques
 
-    if graphique == 'graphique1':
+    if graphique == 'Total Sales by Genre':
         total_sales_by_genre = df.groupby('Genre')['Global_Sales'].sum().sort_values(ascending=False).reset_index()
 
         fig = px.bar(total_sales_by_genre,
@@ -132,9 +135,24 @@ def main():
         st.plotly_chart(fig)
         # Add your description
         # st.markdown("<div class='description'>Text1 here</div>", unsafe_allow_html=True)
-        st.sidebar.text("Analyse du graphique 1: ...")
+        # st.sidebar.text("""Analyse du graphique 1: \
+        # Les jeux de divertissement qui \
+        # demandent moins d'attention \
+        # et de concentration sont beaucoup \
+        # plus vendus que les jeux qui \
+        # demandent une forte concentration. \
+        # Par exemple 3400 millions de jeux d'action et de sport \
+        # ont été vendus tandis que seulement 420 millions \
+        # de jeux de puzzle et de stratégie ont été vendus.""")
+
+        st.sidebar.markdown("""
+        **Analyse du graphique 1:**
+
+        Les jeux de divertissement qui demandent moins d'attention et de concentration sont beaucoup plus vendus que les jeux qui demandent une forte concentration. Par exemple, 3400 millions de jeux d'action et de sport ont été vendus, tandis que seulement 420 millions de jeux de puzzle et de stratégie ont été vendus.
+        """)
+
         
-    elif graphique == 'graphique2':
+    elif graphique == 'Sales by Game Types by Region':
         sales_by_genre_region = df.groupby('Genre')[['NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales']].sum()
 
         most_sold_genre_by_region = sales_by_genre_region.idxmax(axis=0)
@@ -149,40 +167,58 @@ def main():
         plt.style.use("dark_background")
         st.pyplot(fig=plt)
 
-        st.sidebar.text("Analyse du graphique 2: ...")
+        st.sidebar.markdown("""
+        **Analyse du graphique 2:**
         
-    elif graphique == 'graphique3':
+        Aux Etats-Unis et en Europe, les jeux d'actions, de rôle et de sports sont le plus vendus. Tandis qu'au Japon ce sont les jeux de rôle qui sont de loin les plus vendus.
+        """)
+        
+    elif graphique == 'Sales by region':
         sales_by_region = df[['NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales']].sum().reset_index()
         sales_by_region.columns = ['Region', 'Sales']
 
         fig = px.pie(sales_by_region,
                     names='Region',
                     values='Sales',
-                    title='Répartition des Ventes par Région',
+                    title='Sales by region',
                     template='plotly_dark',
                     hole=0.4)
         st.plotly_chart(fig)
 
 
-        st.sidebar.text("Analyse du graphique 3: ...")
+        st.sidebar.markdown("""
+        **Analyse du graphique 3:**
         
-    elif graphique == 'graphique4':
+        L'Amérique du Nord, l'Europe et le Japon représentent 92% des ventes de jeux dans le monde. L'Amérique du Nord représente à elle seule 50% des ventes de jeux.
+        """)
+        
+    elif graphique == 'Top 10 Platforms by Global Sales':
         top_platforms = df.groupby('Platform')['Global_Sales'].sum().nlargest(10).reset_index()
 
         fig = px.bar(top_platforms,
                     x='Platform',
                     y='Global_Sales',
-                    title='Top 10 des Plateformes par Ventes Globales',
-                    labels={'Global_Sales': 'Ventes Globales (en millions)', 'Platform': 'Plateforme'},
+                    title='Top 10 Platforms by Global Sales',
+                    labels={'Global_Sales': 'Global Sales (in millions)', 'Platform': 'Platform'},
                     template='plotly_dark')
 
         
         st.plotly_chart(fig)
-        st.markdown("<div class='description'>Text4 here</div>", unsafe_allow_html=True)
-    elif graphique == 'graphique5':
+        st.sidebar.markdown("""
+        **Analyse du graphique 4:**
+        
+        Au vu des données il vaut mieux sortir un jeu sur console que sur PC.
+        """)
+        # st.markdown("<div class='description'>Text4 here</div>", unsafe_allow_html=True)
+    elif graphique == 'Boxplot of Global Sales by Genre':
         summary_categorical_dist1(df, 'Genre')
-        st.markdown("<div class='description'>Text5 here</div>", unsafe_allow_html=True)
-    elif graphique == 'graphique6':
+        st.sidebar.markdown("""
+        **Analyse du graphique 5:**
+        
+        Les jeux d'action et de sport représentent la majorité des ventes dans le monde.
+        """)
+        # st.markdown("<div class='description'>Text5 here</div>", unsafe_allow_html=True)
+    elif graphique == 'Distribution of Release Years':
         # Create a histogram with Seaborn
         sns.histplot(df['Year_of_Release'].dropna(), bins=30, color='skyblue')
         plt.title("Distribution of Release Years")
@@ -190,6 +226,7 @@ def main():
         plt.ylabel("Count")
         #print the histogram with streamlit
         st.pyplot(fig=plt)
+        
 
         # # Filter data for the year 2010
         games_2010 = df[df['Year_of_Release'] == 2010]
@@ -211,10 +248,15 @@ def main():
 
         # Instead of st.plotly_chart(fig), use st.plotly_chart() without arguments
         st.plotly_chart(fig)
+        st.sidebar.markdown("""
+        **Analyse du graphique 6:**
+        
+        C'est entre 2008 et 2010 que le plus de jeux sont sortis. Au cours de l'année 2010 les jeux d'action, de sports, de tirs et de rôles représentent beaucoup plus de ventes que les jeux de stratégie et d'aventures.
+        """)
 
-        st.markdown("<div class='description'>Text6 here</div>", unsafe_allow_html=True)
+        # st.markdown("<div class='description'>Text6 here</div>", unsafe_allow_html=True)
 
-    elif graphique == 'graphique7':
+    elif graphique == 'Average Global Sales per Region for each Genre':
         sales_col = df.columns[df.columns.str.contains('Sales')]
         fig, axs = plt.subplots(3, 2, figsize=(15, 15))
 
@@ -230,8 +272,14 @@ def main():
         plt.tight_layout()
         st.pyplot(fig=plt)
 
+        st.sidebar.markdown("""
+        **Analyse du graphique 7:**
+        
+        Les jeux de plateformes, de tirs et de rôles représentent le plus de ventes dans le monde. En Amérique du Nord et en Europe c'est les jeux de plateforme et de tirs qui sont le plus vendus. Au Japon c'est principalement les jeux de rôle.
+        """)
+
         # plt.show()
-        st.markdown("<div class='description'>Text7 here</div>", unsafe_allow_html=True)
+        # st.markdown("<div class='description'>Text7 here</div>", unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
